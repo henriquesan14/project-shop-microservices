2165ff061ebb0acf725c82dae7e9c0c9903f2676
Henrique <ricogx@hotmail.com>
2020-08-27 23:14:29 -0300

    first commit - microservices project

---
+++ src/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,53 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if(!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Iphone X",
+                    Summary = "asd",
+                    Description = "asd",
+                    ImageFile = "sas",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Iphone 7",
+                    Summary = "asd",
+                    Description = "asd",
+                    ImageFile = "sas",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Iphone 8",
+                    Summary = "asd",
+                    Description = "asd",
+                    ImageFile = "sas",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                }
+            };
+        }
+    }
+}
