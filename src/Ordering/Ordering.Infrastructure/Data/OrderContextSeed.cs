64dec3ad157f612aacfa6c9ee83b7ae1706bd71d
Henrique <ricogx@hotmail.com>
2020-08-30 19:17:41 -0300

    api ordering

---
+++ src/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILoggerFactory loggerFactory, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+
+            try
+            {
+                orderContext.Database.Migrate();
+
+                if(!orderContext.Orders.Any())
+                {
+                    orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                    await orderContext.SaveChangesAsync();
+                }
+            }catch(Exception exception)
+            {
+                if(retryForAvailability < 3)
+                {
+                    retryForAvailability++;
+                    var log = loggerFactory.CreateLogger<OrderContextSeed>();
+                    log.LogError(exception.Message);
+                    await SeedAsync(orderContext, loggerFactory, retryForAvailability);
+                }
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "swn", FirstName = "Mehmet", LastName = "Ozkaya", EmailAddress = "wsasas@gmail.com", AddressLine = "Bahspsaps", Country = "Turkey"}
+            };
+        }
+    }
+}
