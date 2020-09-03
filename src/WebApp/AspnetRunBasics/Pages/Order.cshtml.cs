f7b3de11acadcd3e8400ff022028c2c9046459c5
Henrique <ricogx@hotmail.com>
2020-09-02 22:50:12 -0300

    webapp aspnet

---
+++ src/WebApp/AspnetRunBasics/Pages/Order.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspnetRunBasics.ApiCollection.Interfaces;
+using AspnetRunBasics.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspnetRunBasics
+{
+    public class OrderModel : PageModel
+    {
+        private readonly IOrderApi _orderAPi;
+
+        public OrderModel(IOrderApi orderAPi)
+        {
+            _orderAPi = orderAPi;
+        }
+
+        public IEnumerable<OrderResponseModel> Orders { get; set; } = new List<OrderResponseModel>();
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Orders = await _orderAPi.GetOrdersByUserName("swn");
+
+            return Page();
+        }       
+    }
+}
